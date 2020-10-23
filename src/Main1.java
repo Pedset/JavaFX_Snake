a43bfc4f690d8c11518253af162363e0cfdd635c
Fatmir87 <56822851+Fatmir87@users.noreply.github.com>
2020-10-23 20:42:47 +0200

    First

---
+++ src/Main1.java
@@ -0,0 +1,9 @@
+public class Main1 extends Application {
+
+    static int block_size = 20;
+    int field_width = 70, field_height = 35;
+    Field field;
+
+    public static void main(String[] args) {
+		launch(args);
+	}
