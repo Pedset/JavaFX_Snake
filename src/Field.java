44255d7f2fa5e36ac53316c7072c00b93039709f
Bare758 <Abdulahial413@gmail.co>
2020-10-23 21:33:12 +0200

    update

--- src/Field.java
+++ src/Field.java
@@ -1,6 +1,13 @@
 import java.util.ArrayList;
 
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class Field extends Pane {
 	private int w, h;
@@ -19,6 +26,16 @@ public class Field extends Pane {
 
 		getChildren().add(b);
 		blocks.add(b);
+	}
+	
+	public Field(int width, int height) {
+		w= width;
+		h= height;
+		//
+		setMinSize(w * Main1.block_size, h * Main1.block_size);
+		setBackground(new Background(new BackgroundFill(Color.BLACK,null, null)));
+		setBorder(new Border(new BorderStroke(Color.WHITE,BorderStrokeStyle.SOLID, null, new BorderWidths(1))));
+		
 	}
 	
 }
--- src/Main1.java
+++ src/Main1.java
@@ -1,4 +1,9 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main1 extends Application {
@@ -14,6 +19,18 @@ public class Main1 extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		
+		VBox verticalBox = new VBox(10);
+		verticalBox.setPadding(new Insets(5));
 		
+		//Creates field
+		field = new Field(field_width, field_height);
+		Label score = new Label("Score: 0");
+		score.setFont(Font.font("tahoma", 20));
+		verticalBox.getChildren().addAll(score, field);
+		Scene scene = new Scene(verticalBox);
+		
+		stage.setResizable(false);
+		stage.setScene(scene);
+		stage.show();
 	}
 }
