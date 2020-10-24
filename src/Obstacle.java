fa1c7bde0421b19c402071090bb4622ece86178e
Pedset <57757244+Pedset@users.noreply.github.com>
2020-10-24 11:00:57 +0200

    obs v1

---
+++ src/Obstacle.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Obstacle {
+		
+	ArrayList<Block> blocks = new ArrayList<>();
+	
+	public Obstacle(Field f) {
+			ArrayList<ArrayList<Integer>> newCords = generateObs(f);
+		}
+	
+	
+	
+	
+	
+	public ArrayList<ArrayList<Integer>> generateObs (Field f){
+		
+		ArrayList<ArrayList<Integer>> obsCord = new ArrayList<>();
+		
+		int[] randomCords;
+		
+		for (int z = 0 ; z < 2; z++) {   																		 // 2 times (1 series vertical and 1 series horizontal)
+			for (int a=1; a <= 4 ; a++) {   																	 // 4 lines 
+				int[] maxs = {(f.getW()-1),(f.getH()-1)};
+				randomCords= new int[2];
+					for (int b=0; b < 2; b++) {
+						int max = ((maxs[b]-10)/2);
+				        int min = 0;
+				        int range = max - min + 1;
+				        randomCords[b] = (int)(Math.random() * range) + min;
+				        if(a==2 && b==0) randomCords[b]+=(69/2);
+						else if(a==3 && b==1) randomCords[b]+=(29/2);
+						else if(a==4 && b==0) randomCords[b]+=(69/2);
+						else if(a==4 && b==1) randomCords[b]+=(29/2);
+						}
+					for (int c = 0; c <= ((int)(Math.random() * (10 - 5 + 1)) + 5); c++) { 						 //random length between 10 and 5
+						ArrayList<Integer> cords = new ArrayList<>();
+						cords.add(randomCords[0]);
+						cords.add(randomCords[1]);
+						randomCords[z]=randomCords[z]+1;
+						obsCord.add(cords);
+						}	
+				}
+		}
+		return obsCord;
+	}
+}
